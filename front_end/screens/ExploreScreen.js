d9008185b06fc276377680d1e691e71c28e9246f
hetpatel4902 <88403427+hetpatel4902@users.noreply.github.com>
2022-02-26 20:29:37 +0530

    het

---
+++ front_end/screens/ExploreScreen.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ExploreScreen = () => {
+  return (
+    <View>
+      <Text>ExploreScreen</Text>
+    </View>
+  );
+};
+
+export default ExploreScreen;
